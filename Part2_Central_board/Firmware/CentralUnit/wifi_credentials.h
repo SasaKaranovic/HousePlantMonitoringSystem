207c8598f094bef16f957f574378fb18961500ef
Sasa Karanovic <75916+SasaKaranovic@users.noreply.github.com>
2021-10-22 00:32:28 -0400

    Update wifi_credentials.h

--- Part2_Central_board/Firmware/CentralUnit/wifi_credentials.h
+++ Part2_Central_board/Firmware/CentralUnit/wifi_credentials.h
@@ -1,7 +1,7 @@
 #ifndef __WIFI_CRED_CFG
 #define __WIFI_CRED_CFG
 
-const char* ssid = "Koshon";
-const char* password =  "sxdcfvgbhnjmk";
+const char* ssid = "Your-SSID";
+const char* password =  "Your-WiFi-Password";
 
 #endif
